8e523b6993c413b2e7e34fe52f4e3ad424df78a0
LetzParty <james.201195@gmail.com>
2017-12-08 17:07:22 +0000

    Add project page

--- server.js
+++ server.js
@@ -48,8 +48,14 @@ app.get("/about", (req, res) => {
   });
 });
 
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    pageTitle: "Portfolio Page Here"
+  });
+});
+
 app.get("/bad", (req, res) => {
-  res.send({
+  res.sed({
     errorMessage: "Unable to fulfil request :("
   });
 });
